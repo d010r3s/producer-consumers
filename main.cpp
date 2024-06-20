2c856dbafbd910aef22a5fdb24647d68c41d7230
Sadbery <126723647+sadberyy@users.noreply.github.com>
2024-06-20 12:43:59 +0300

    Update random time for processes

--- main.cpp
+++ main.cpp
@@ -4,6 +4,9 @@
 #include <mutex>
 #include <condition_variable>
 #include <chrono>
+#include <random>
+
+using namespace std;
 
 class Order {
 public:
@@ -13,20 +16,25 @@ public:
 
 class OnlineOrder : public Order {
 private:
-    std::mutex& io_mutex;
+    mutex& io_mutex;
 public:
-    OnlineOrder(std::mutex& mtx) : io_mutex(mtx) {}
+    OnlineOrder(mutex& mtx) : io_mutex(mtx) {}
 
     bool process() override {
-        int processTime = rand() % 5 + 1;
+        // Генератор случайных чисел и распределение для каждого объекта
+        random_device rd;
+        mt19937 gen(rd());
+        uniform_int_distribution<> dis(1, 5);
+        int processTime = dis(gen);
+
         {
-            std::lock_guard<std::mutex> lock(io_mutex);
-            std::cout << "Processing for " << processTime << " seconds..." << std::endl;
+            lock_guard<mutex> lock(io_mutex);
+            cout << "Processing for " << processTime << " seconds..." << endl;
         }
-        std::this_thread::sleep_for(std::chrono::seconds(processTime));
+        this_thread::sleep_for(chrono::seconds(processTime));
         {
-            std::lock_guard<std::mutex> lock(io_mutex);
-            std::cout << "Complete" << std::endl;
+            lock_guard<mutex> lock(io_mutex);
+            cout << "Complete" << endl;
         }
         return true;
     }
@@ -35,18 +43,18 @@ public:
 // Блокирующая очередь
 class BlockingQueue {
 private:
-    std::queue<Order*> queue;
-    std::mutex mtx;
-    std::condition_variable cv;
+    queue<Order*> queue;
+    mutex mtx;
+    condition_variable cv;
 public:
     void put(Order* item) {
-        std::unique_lock<std::mutex> lock(mtx);
+        unique_lock<mutex> lock(mtx);
         queue.push(item);
         cv.notify_one();
     }
 
     Order* take() {
-        std::unique_lock<std::mutex> lock(mtx);
+        unique_lock<mutex> lock(mtx);
         cv.wait(lock, [this] { return !queue.empty(); });
         Order* item = queue.front();
         queue.pop();
@@ -59,17 +67,17 @@ class OrderProducer {
 private:
     BlockingQueue& queue;
     int numberOfOrders;
-    std::mutex& io_mutex;
+    mutex& io_mutex;
 public:
-    OrderProducer(BlockingQueue& q, int num, std::mutex& mtx) : queue(q), numberOfOrders(num), io_mutex(mtx) {}
+    OrderProducer(BlockingQueue& q, int num, mutex& mtx) : queue(q), numberOfOrders(num), io_mutex(mtx) {}
 
     void operator()() {
         for (int i = 0; i < numberOfOrders; ++i) {
             Order* order = new OnlineOrder(io_mutex);
             queue.put(order);
             {
-                std::lock_guard<std::mutex> lock(io_mutex);
-                std::cout << "Created order " << i + 1 << std::endl;
+                lock_guard<mutex> lock(io_mutex);
+                cout << "Created order " << i + 1 << endl;
             }
         }
     }
@@ -79,17 +87,17 @@ public:
 class OrderConsumer {
 private:
     BlockingQueue& queue;
-    std::string name;
-    std::mutex& io_mutex;
+    string name;
+    mutex& io_mutex;
 public:
-    OrderConsumer(BlockingQueue& q, const std::string& n, std::mutex& mtx) : queue(q), name(n), io_mutex(mtx) {}
+    OrderConsumer(BlockingQueue& q, const string& n, mutex& mtx) : queue(q), name(n), io_mutex(mtx) {}
 
     void operator()() {
         while (true) {
             Order* order = queue.take();
             {
-                std::lock_guard<std::mutex> lock(io_mutex);
-                std::cout << name << " is processing an order..." << std::endl;
+                lock_guard<mutex> lock(io_mutex);
+                cout << name << " is processing an order..." << endl;
             }
             order->process();
             delete order;
@@ -100,22 +108,22 @@ public:
 int main() {
     srand(time(0));
     BlockingQueue queue;
-    std::mutex io_mutex;
+    mutex mtx;
 
-    OrderProducer producer(queue, rand() % 10 + 5, io_mutex);
-    std::thread producerThread(producer);
-    
-    OrderConsumer consumer1(queue, "Consumer 1", io_mutex);
-    OrderConsumer consumer2(queue, "Consumer 2", io_mutex);
-    std::thread consumerThread1(consumer1);
-    std::thread consumerThread2(consumer2);
-    
+    OrderProducer producer(queue, rand() % 10 + 5, mtx);
+    thread producerThread(producer);
+
+    OrderConsumer consumer1(queue, "Consumer 1", mtx);
+    OrderConsumer consumer2(queue, "Consumer 2", mtx);
+    thread consumerThread1(consumer1);
+    thread consumerThread2(consumer2);
+
     producerThread.join();
-    
+
     consumerThread1.detach();
     consumerThread2.detach();
 
-    std::this_thread::sleep_for(std::chrono::seconds(60)); 
+    this_thread::sleep_for(chrono::seconds(30));
 
     return 0;
 }
